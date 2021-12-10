a5d3aaa872d69fff5981d9d24cf1faaf88038e5d
Jeremy Matt <jeremy.matt@gmail.com>
2021-12-10 13:50:35 -0500

    Start working on controller

---
+++ coop_controller.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 10 11:34:53 2021
+
+@author: jmatt
+"""
+
+import time
+import coop_functions as CF
--- coop_functions.py
+++ coop_functions.py
@@ -6,7 +6,7 @@ Created on Tue Nov 23 08:43:37 2021
 """
 
 import datetime as dt
-t = dt.datetime.utcnow()
+from suntime import Sun, SunTimeException
 
 
 import smtplib
@@ -14,21 +14,46 @@ import settings
 from email.message import EmailMessage
 
 
-def send_notification(message):
+
+class coop_controller:
     
-    msg= EmailMessage()
-    
-    app_generated_password = settings.password    # gmail generated password
-    msg["From"]= 'coop controller'      #sender address
-    
-    msg["To"] = settings.phone_number     #reciver address
-    
-    msg.set_content(message)   #message body
-    
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+    def __init__(self):
+        self.get_cur_time
+        self.cur_day = self.cur_day.day
+            
         
-        smtp.login(my_address,app_generated_password)    #login gmail account
         
-        print("sending mail")
-        smtp.send_message(msg)   #send message 
-        print("mail has sent")
+    def init_pins(self):
+        
+        
+    def get_cur_time(self):
+        self.cur_time = dt.datetime.now(dt.timezone.utc)
+    
+    def get_sunrise_sunset(self):
+        sun = Sun(settings.latitude, settings.longitude)
+        self.today_sr = sun.get_sunrise_time(self.cur_time)
+        self.today_ss = sun.get_sunset_time(self.cur_time)
+        
+        self.close_time = self.today_ss + dt.timedelta(minutes=settings.wait_after_sunset)
+        
+        
+    
+    
+    def send_notification(message):
+        
+        msg= EmailMessage()
+        
+        app_generated_password = settings.password    # gmail generated password
+        msg["From"]= 'coop controller'      #sender address
+        
+        msg["To"] = settings.phone_number     #reciver address
+        
+        msg.set_content(message)   #message body
+        
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+            
+            smtp.login(my_address,app_generated_password)    #login gmail account
+            
+            print("sending mail")
+            smtp.send_message(msg)   #send message 
+            print("mail has sent")
