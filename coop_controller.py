8e6b6e2e2f93c939edd02702d2b2fb2ca361222c
Jeremy Matt <jeremy.matt@gmail.com>
2021-12-13 20:22:37 -0500

    debug

--- coop_controller.py
+++ coop_controller.py
@@ -20,7 +20,8 @@ s = str(dt.second).zfill(2)
 # sys.stdout = open('{}-{}-{}_{}-{}-{}_log.txt'.format(y,m,d,h,m,s), 'w')
 
 controller = CF.coop_controller()
-try:
-    controller.run()
-except:
-    CF.send_crash_notification()
+controller.run()
+# try:
+#     controller.run()
+# except:
+#     CF.send_crash_notification()
