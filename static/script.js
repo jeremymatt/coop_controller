6e63f0bdf3e018928fff98bf04ee6dc643300cfe
Jeremy Matt <jeremy.matt@gmail.com>
2024-12-27 14:03:34 -0500

    u

--- static/script.js
+++ static/script.js
@@ -1,41 +1,42 @@
-function sendCommand(command) {
-    console.log(`Sending command: ${command}`);
-    fetch("/update", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ command }),
-    })
-    .then(response => {
-        console.log(`Response status: ${response.status}`);
-        return response.json();
-    })
-    .then(data => {
-        console.log("Response data:", data);
-        updatePage(data);
-    })
-    .catch(error => console.error("Error sending command:", error));
-}
 
-
-function updatePage(data) {
-    // Update text fields
-    document.getElementById("door_current_state").innerText = data.door_current_state;
-    document.getElementById("door_error_state").innerText = data.door_error_state;
-    document.getElementById("door_auto_state").innerText = data.door_auto_state;
-
-    document.getElementById("light_current_state").innerText = data.light_current_state;
-    document.getElementById("light_auto_state").innerText = data.light_auto_state;
-
-    document.getElementById("system_time").innerText = data.system_time;
-}
-
-function fetchUpdate() {
-    sendCommand("update");
-}
 
 document.addEventListener("DOMContentLoaded", () => {
     const updateInterval = 1000; // Update every second
 
+    function sendCommand(command) {
+        console.log(`Sending command: ${command}`);
+        fetch("/update", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ command }),
+        })
+        .then(response => {
+            console.log(`Response status: ${response.status}`);
+            return response.json();
+        })
+        .then(data => {
+            console.log("Response data:", data);
+            updatePage(data);
+        })
+        .catch(error => console.error("Error sending command:", error));
+    }
+    
+    
+    function updatePage(data) {
+        // Update text fields
+        document.getElementById("door_current_state").innerText = data.door_current_state;
+        document.getElementById("door_error_state").innerText = data.door_error_state;
+        document.getElementById("door_auto_state").innerText = data.door_auto_state;
+    
+        document.getElementById("light_current_state").innerText = data.light_current_state;
+        document.getElementById("light_auto_state").innerText = data.light_auto_state;
+    
+        document.getElementById("system_time").innerText = data.system_time;
+    }
+    
+    function fetchUpdate() {
+        sendCommand("update");
+    }
 
 
     // Attach event listeners to buttons
