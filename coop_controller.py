92ae20d50339ecdb58bbf6cf659deec37b79071d
Jeremy Matt <jeremy.matt@gmail.com>
2024-12-27 13:45:30 -0500

    u

--- coop_controller.py
+++ coop_controller.py
@@ -93,6 +93,70 @@ def signal_handler(sig, frame):
 #                     controller.cancel_error()
 #                 response_queue.put(controller.return_current_state())
 
+# Create queues for communication
+global command_queue
+global response_queue
+command_queue = Queue()
+response_queue = Queue()
+
+app = Flask(__name__)
+# CORS(app)  # Allow CORS for all routes
+app.secret_key = 'your_secret_key_here'  # Replace with a strong secret key
+
+# Load credentials
+with open('credentials.config', 'r') as file:
+    credentials = file.read().splitlines()
+    USERNAME_HASH = credentials[0].strip()
+    PASSWORD_HASH = credentials[1].strip()
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if (hashlib.md5(username.encode()).hexdigest() == USERNAME_HASH and
+                hashlib.md5(password.encode()).hexdigest() == PASSWORD_HASH):
+            session["authenticated"] = True
+            return redirect("/")
+
+    if session.get("authenticated"):
+        return render_template("template.html")
+    else:
+        return render_template('login.html')    
+        
+@app.route('/update', methods=['GET','POST'])
+def update():
+    if request.method == "POST":
+        action = request.json.get('command')
+        print('received post request with action of: {}'.format(action))
+        action = request.json.get('action')
+        command_queue.put(action)
+        while response_queue.empty():
+            time.sleep(0.1)
+        data = response_queue.get()
+        print(data)
+        return jsonify(data)
+    elif request.method == "GET":
+        print('received get request')
+        return  '''
+                <!DOCTYPE html>
+                <html>
+                    <head>
+                        <h1> HELLO WORLD </h1>
+                    </head>
+                    <body>
+                        here be chickens
+                    </body>
+                </html>'''
+
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.clear()
+    return redirect("/")
+
 if __name__ == '__main__':
     port = 8000
     ngrok_static_url = 'chickencoop.fun'
@@ -104,7 +168,8 @@ if __name__ == '__main__':
         exit(1)
 
         
-    controller_process = Process(target=CF.run_coop_controller, args=(CF.command_queue, CF.response_queue))
+    # controller_process = Process(target=CF.run_coop_controller, args=(CF.command_queue, CF.response_queue))
+    controller_process = Process(target=run_coop_controller, args=(command_queue, response_queue))
     controller_process.start()
 
     # Start Flask app
