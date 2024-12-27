ac64575ae244a6ee34deb9212a9089bc8a068b33
Jeremy Matt <jeremy.matt@gmail.com>
2024-12-27 14:14:32 -0500

    u

--- static/script.js
+++ static/script.js
@@ -1,41 +1,41 @@
+function sendCommand(command,document) {
+    console.log(`Sending command: ${command}`);
+    fetch("/update", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ command }),
+    })
+    .then(response => {
+        console.log(`Response status: ${response.status}`);
+        return response.json();
+    })
+    .then(data => {
+        console.log("Response data:", data);
+        updatePage(data,document);
+    })
+    .catch(error => console.error("Error sending command:", error));
+}
+
+
+function updatePage(data,document) {
+    // Update text fields
+    document.getElementById("door_current_state").innerText = data.door_current_state;
+    document.getElementById("door_error_state").innerText = data.door_error_state;
+    document.getElementById("door_auto_state").innerText = data.door_auto_state;
+
+    document.getElementById("light_current_state").innerText = data.light_current_state;
+    document.getElementById("light_auto_state").innerText = data.light_auto_state;
+
+    document.getElementById("system_time").innerText = data.system_time;
+}
 
 
 document.addEventListener("DOMContentLoaded", () => {
     const updateInterval = 1000; // Update every second
 
-    function sendCommand(command) {
-        console.log(`Sending command: ${command}`);
-        fetch("/update", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ command }),
-        })
-        .then(response => {
-            console.log(`Response status: ${response.status}`);
-            return response.json();
-        })
-        .then(data => {
-            console.log("Response data:", data);
-            updatePage(data);
-        })
-        .catch(error => console.error("Error sending command:", error));
-    }
-    
-    
-    function updatePage(data) {
-        // Update text fields
-        document.getElementById("door_current_state").innerText = data.door_current_state;
-        document.getElementById("door_error_state").innerText = data.door_error_state;
-        document.getElementById("door_auto_state").innerText = data.door_auto_state;
-    
-        document.getElementById("light_current_state").innerText = data.light_current_state;
-        document.getElementById("light_auto_state").innerText = data.light_auto_state;
-    
-        document.getElementById("system_time").innerText = data.system_time;
-    }
     
     function fetchUpdate() {
-        sendCommand("update");
+        sendCommand("update",document);
     }
 
 
