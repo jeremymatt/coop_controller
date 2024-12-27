2d93d5b9983911a173ac0bc61502030dabb27adc
Jeremy Matt <jeremy.matt@gmail.com>
2024-12-27 14:01:11 -0500

    u

--- static/script.js
+++ static/script.js
@@ -16,24 +16,27 @@ function sendCommand(command) {
     .catch(error => console.error("Error sending command:", error));
 }
 
+
+function updatePage(data) {
+    // Update text fields
+    document.getElementById("door_current_state").innerText = data.door_current_state;
+    document.getElementById("door_error_state").innerText = data.door_error_state;
+    document.getElementById("door_auto_state").innerText = data.door_auto_state;
+
+    document.getElementById("light_current_state").innerText = data.light_current_state;
+    document.getElementById("light_auto_state").innerText = data.light_auto_state;
+
+    document.getElementById("system_time").innerText = data.system_time;
+}
+
+function fetchUpdate() {
+    sendCommand("update");
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     const updateInterval = 1000; // Update every second
 
-    function fetchUpdate() {
-        sendCommand("update");
-    }
 
-    function updatePage(data) {
-        // Update text fields
-        document.getElementById("door_current_state").innerText = data.door_current_state;
-        document.getElementById("door_error_state").innerText = data.door_error_state;
-        document.getElementById("door_auto_state").innerText = data.door_auto_state;
-
-        document.getElementById("light_current_state").innerText = data.light_current_state;
-        document.getElementById("light_auto_state").innerText = data.light_auto_state;
-
-        document.getElementById("system_time").innerText = data.system_time;
-    }
 
     // Attach event listeners to buttons
     document.querySelectorAll("button[data-command]").forEach(button => {
