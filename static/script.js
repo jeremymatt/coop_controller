9e775a6036f8934a87cc7bac56ddbb5917a66320
Jeremy Matt <jeremy.matt@gmail.com>
2024-12-28 20:36:27 -0500

    u

--- static/script.js
+++ static/script.js
@@ -82,5 +82,5 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     // Start the update loop
-    // setInterval(fetchUpdate, updateInterval);
+    setInterval(fetchUpdate, updateInterval);
 });
