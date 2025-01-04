dda8d620044168f8dbac6861277080c921531e55
Jeremy Matt <jeremy.matt@gmail.com>
2025-01-04 16:43:26 -0500

    update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	&& sudo apt update \
 	&& sudo apt install ngrok
 
-python -m venv ~/.venv
+virtualenv -q -p /usr/bin/python3.5 ~/.venv
 source ~/.venv/bin/activate
 pip install -r requirements.txt
