64c55b16b55d2dacb4744c01a7f769816a047850
Jeremy Matt <jeremy.matt@gmail.com>
2025-01-04 16:46:45 -0500

    update test isntall script

--- test_install.sh
+++ test_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install virtualenv
+sudo apt-get install virtualenv -y
 virtualenv -q -p /usr/bin/python3.5 ~/.venv
 source ~/.venv/bin/activate
 pip install -r requirements.txt
