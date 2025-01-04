f4b01dc88f5f8e5ec56dafcff79d0720c8883d3a
Jeremy Matt <jeremy.matt@gmail.com>
2025-01-04 17:06:02 -0500

    update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 	&& sudo apt update \
 	&& sudo apt install ngrok
 
-virtualenv -q -p /usr/bin/python3.5 ~/.venv
-source ~/.venv/bin/activate
+sudo apt-get install virtualenv -y
+virtualenv -q -p /usr/bin/python $HOME/.venv
+source $HOME/.venv/bin/activate
 pip install -r requirements.txt
+sudo sudo raspi-config nonint do_i2c 0
--- test_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get install virtualenv -y
-virtualenv -q -p /usr/bin/python $HOME/.venv
-source $HOME/.venv/bin/activate
-pip install -r requirements.txt
