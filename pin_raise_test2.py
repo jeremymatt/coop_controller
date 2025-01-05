852233185b4e4b90aa7a3a1da69762c26c8fdf65
Jeremy Matt <jeremy.matt@gmail.com>
2025-01-04 20:37:15 -0500

    clean up directory

--- pin_raise_test2.py
+++
@@ -1,5 +0,0 @@
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-gpio_number = 12
-GPIO.setup(gpio_number, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-print("GPIO no " + str(gpio_number) + ": " + str(GPIO.input(gpio_number)))
